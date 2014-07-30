6797a222b6c0b1e5fed63f646cf6a6cf13d3ba66
Kenton Varda <temporal@gmail.com>
2014-07-29 22:04:24 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Sandstorm.io login integration for Meteor.js
+
+[Sandstorm](https://sandstorm.io) is a platform for personal clouds that makes
+installing apps to your personal server as easy as installing apps to your
+phone.
+
+[Meteor](https://meteor.com) is a revolutionary web app framework. Sandstorm's
+own UI is built using Meteor, and Meteor is also a great way to build Sandstorm
+apps.
+
+This package is meant to be used by Meteor apps built to run on Sandstorm.
+It integrates with Sandstorm's built-in login system to log the user in
+automatically when they open the app. The user's `profile.name` will be
+populated from Sandstorm. When using this package, you should not use
+`accounts-ui` at all; just let login happen automatically.
+
+To package a Meteor app for Sandstorm,
+[use the `meteor-spk` tool](https://github.com/sandstorm-io/meteor-spk).
+
---
+++ server.js
@@ -0,0 +1,69 @@
+// Copyright (c) 2014 Sandstorm Development Group, Inc. and contributors
+// Licensed under the MIT License:
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+WebApp.rawConnectHandlers.use(function (req, res, next) {
+  if (req.url === "/.sandstorm-credentials") {
+    handleCredentials(req, res);
+    return;
+  }
+  return next();
+});
+
+var handleCredentials = Meteor.bindEnvironment(function (req, res) {
+  try {
+    var permissions = req.headers["x-sandstorm-permissions"];
+    if (permissions && permissions !== "") {
+      permissions = permissions.split(",");
+    } else {
+      permissions = [];
+    }
+
+    var credentials = {
+      sandstormId: req.headers["x-sandstorm-user-id"] || null,
+      name: req.headers["x-sandstorm-username"],
+      permissions: permissions
+    };
+
+    if (credentials.sandstormId) {
+      var login = Accounts.updateOrCreateUserFromExternalService(
+          "sandstorm", { id: credentials.sandstormId },
+          { profile: { name: credentials.name } });
+      console.log(login);
+      credentials.meteorId = login.userId;
+      var token = Accounts._generateStampedLoginToken();
+      credentials.token = token.token;
+      Accounts._insertLoginToken(login.userId, token);
+    }
+
+    var text = JSON.stringify(credentials);
+
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+      "Content-Length": text.length
+    });
+    res.end(text);
+  } catch (err) {
+    res.writeHead(500, {
+      "Content-Type": "text/plain"
+    });
+    res.end(err.stack);
+  }
+});
