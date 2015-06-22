a12026d6317169ac1d6b9385d508cb8061e21dd2
Kenton Varda <kenton@sandstorm.io>
2015-06-21 19:46:31 -0700

    Bump version for release.

--- package.js
+++ package.js
@@ -21,7 +21,7 @@
 
 Package.describe({
   summary: "Login service for Sandstorm.io applications",
-  version: "0.1.3",
+  version: "0.1.4",
   name: "kenton:accounts-sandstorm",
   git: "https://github.com/sandstorm-io/meteor-accounts-sandstorm.git"
 });
