cbd8c76dfd692cdaec0d89b24b40ca415fc402ad
Kenton Varda <kenton@sandstorm.io>
2016-03-03 03:03:30 -0800

    Version 0.2: MAJOR UPDATE:
    - Permissions are now available for anonymous users.
    - You don't even need Meteor accounts anymore (if you don't need to record past users).
    - Testability outside of Sandstorm is built-in.
    - Faster (fewer round trips).
    - More robust.

--- client.js
+++ client.js
@@ -19,25 +19,157 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-Meteor.startup(function () {
-  Deps.autorun(function () {
-    if (!Meteor.userId()) {
-      // Accounts._setLoggingIn is a semi-private function, but it's also used in accounts-password.
-      Accounts._setLoggingIn(true);
-      HTTP.get("/.sandstorm-credentials", function (error, result) {
-        if (error) {
-          Accounts._setLoggingIn(false);
-          console.error(error.stack);
-        } else if (!result.data) {
-          Accounts._setLoggingIn(false);
-          console.error("/.sandstorm-credentials is not JSON?");
-        } else if (result.data.token) {
-          // Don't call Accounts._setLoggingIn here and let the normal login flow take over.
-          Meteor.loginWithToken(result.data.token, function() {});
-        } else {
-          Accounts._setLoggingIn(false);
+function loginWithSandstorm(connection) {
+  // Log in the connection using Sandstorm authentication.
+  //
+  // After calling this, connection.sandstormUser() will reactively return an object containing
+  // Sansdstorm user info, including permissions as authenticated by the server. Even if the user
+  // is anonymous, this information is returned. `sandstormUser()` returns `null` up until the
+  // point where login succeeds.
+
+  // How this works:
+  // 1. We cerate a cryptographically random token, which we're going to send to the server twice.
+  // 2. We make a method call to log in with this token. The server initially has no idea who
+  //    is calling and will block waiting for info. (The method is marked "wait" on the client side
+  //    so that further method calls are blocked until login completes.)
+  // 3. We also send an XHR with the same token. When the server receives the XHR, it harvests the
+  //    Sandstorm headers, looks for the corresponding login method call, marks its connection as
+  //    logged in, and then lets it return.
+  //
+  // We don't actually use Accounts.callLoginMethod() because we don't need or want the
+  // "resume token" logic. On a disconnect, we need to re-authenticate, because the user's
+  // permissions may have changed (indeed, this may be the reason for the disconnect).
+
+  // If the connection doesn't alerady have a sandstormUser() method, add it now.
+  if (!connection._sandstormUser) {
+    connection._sandstormUser = new ReactiveVar(null);
+    connection.sandstormUser = connection._sandstormUser.get.bind(connection._sandstormUser);
+  }
+
+  // Generate a random token which we'll send both over an XHR and over DDP at the same time.
+  var token = Random.secret();
+
+  var waiting = true;          // We'll keep retrying XHRs until the method returns.
+  var reconnected = false;
+
+  var onResultReceived = function (error, result) {
+    waiting = false;
+
+    if (error) {
+      // ignore for now; loggedInAndDataReadyCallback() will get the error too
+    } else {
+      connection.onReconnect = function () {
+        reconnected = true;
+        loginWithSandstorm(connection);
+      };
+    }
+  };
+
+  var loggedInAndDataReadyCallback = function (error, result) {
+    if (reconnected) {
+      // Oh, we're already on a future connection attempt. Don't mess with anything.
+      return;
+    }
+
+    if (error) {
+      console.error("loginWithSandstorm failed:", error);
+    } else {
+      connection._sandstormUser.set(result.sandstorm);
+      connection.setUserId(result.userId);
+    }
+  };
+
+  Meteor.apply("loginWithSandstorm", [token],
+      {wait: true, onResultReceived: onResultReceived},
+      loggedInAndDataReadyCallback);
+
+  var sendXhr = function () {
+    if (!waiting) return;  // Method call finished.
+
+    headers = {"Content-Type": "application/x-sandstorm-login-token"};
+
+    var testInfo = localStorage.sandstormTestUserInfo;
+    if (testInfo) {
+      testInfo = JSON.parse(testInfo);
+      if (testInfo.id) {
+        headers["X-Sandstorm-User-Id"] = testInfo.id;
+      }
+      if (testInfo.name) {
+        headers["X-Sandstorm-Username"] = encodeURI(testInfo.name);
+      }
+      if (testInfo.picture) {
+        headers["X-Sandstorm-User-Picture"] = testInfo.picture;
+      }
+      if (testInfo.permissions) {
+        headers["X-Sandstorm-Permissions"] = testInfo.permissions.join(",");
+      }
+      if (testInfo.preferredHandle) {
+        headers["X-Sandstorm-Preferred-Handle"] = testInfo.preferredHandle;
+      }
+      if (testInfo.pronouns) {
+        headers["X-Sandstorm-User-Pronouns"] = testInfo.pronouns;
+      }
+    }
+
+    // Send the token in an HTTP POST request which on the server side will allow us to receive the
+    // Sandstorm headers.
+    HTTP.post("/.sandstorm-login",
+        {content: token, headers: headers},
+        function (error, result) {
+      if (error) {
+        console.error("couldn't get /.sandstorm-login:", error);
+
+        if (waiting) {
+          // Try again in a second.
+          Meteor.setTimeout(sendXhr, 1000);
         }
-      });
+      }
+    });
+  }
+
+  // Wait until the connection is up before we start trying to send XHRs.
+  var stopImmediately = false;  // Unfortunately, Tracker.autorun() runs the first time inline.
+  var handle = Tracker.autorun(function () {
+    if (!waiting) {
+      if (handle) {
+        handle.stop();
+      } else {
+        stopImmediately = true;
+      }
+      return;
+    } else if (connection.status().connected) {
+      if (handle) {
+        handle.stop();
+      } else {
+        stopImmediately = true;
+      }
+
+      // Wait 10ms before our first attempt to send the rendezvous XHR because if it arrives
+      // before the method call it will be rejected.
+      Meteor.setTimeout(sendXhr, 10);
     }
   });
-});
+  if (stopImmediately) handle.stop();
+}
+
+// Auto-login the main Meteor connection.
+loginWithSandstorm(Meteor.connection);
+
+if (Package["accounts-base"]) {
+  // Make Meteor.loggingIn() work by calling a private method of accounts-base. If this breaks then
+  // maybe we should just overwrite Meteor.loggingIn() instead.
+  Tracker.autorun(function () {
+    Package["accounts-base"].Accounts._setLoggingIn(!Meteor.connection.sandstormUser());
+  });
+}
+
+Meteor.sandstormUser = function () {
+  return Meteor.connection.sandstormUser();
+}
+
+SandstormAccounts = {
+  setTestUserInfo: function (info) {
+    localStorage.sandstormTestUserInfo = JSON.stringify(info);
+    loginWithSandstorm(Meteor.connection);
+  }
+};
